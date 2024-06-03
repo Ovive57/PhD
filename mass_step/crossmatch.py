619971c6545e8dca5bbaf2704cb112c653e2c74b
Olivia <ovidalvelazquez@swin.edu.au>
2024-06-03 14:50:51 +1000

    Script and catalogues ordered, the script needs still work

--- mass_step/crossmatch.py
+++ mass_step/crossmatch.py
@@ -12,48 +12,9 @@ plt.style.use(style.style1)
 gama_file = "data/gkvScienceCatv02.fits"
 ztf_file = "data/tns_SNIa_20240424_copy.csv"
 
-'''
-ztf_ra = np.char.strip(np.loadtxt(ztf_file, dtype=str, skiprows=1, usecols=(2), unpack=True, delimiter=","),'"') #hourangle
-ztf_dec = np.char.strip(np.loadtxt(ztf_file, dtype=str, skiprows=1, usecols=(3), unpack=True, delimiter=","),'"') #hourangle
-ztf_ra1 = ztf_ra[0]
-ztf_dec1 = ztf_dec[0]
-'''
 
 #https://docs.astropy.org/en/stable/coordinates/index.html
 
-def separations(cat_file1, cat_file2, max_sep, frame = 'icrs', test = False):
-    start = time.time()
-    cat1 = Table.read(cat_file1)
-    cat2 = Table.read(cat_file2)
-    radec1 = co.SkyCoord(cat1['RAcen'], cat1['Deccen'], frame = frame, unit='deg') # GAMA (ICRS): (ra, dec) in deg
-    radec2 = co.SkyCoord(cat2['ra']*u.deg, cat2['declination']*u.deg, frame=frame) # ZTF (ICRS): (ra, dec) in deg
-
-    max_sep = max_sep*u.arcsec
-
-    result_table = Table(names=cat1.colnames + ['objid', 'Separation_arcsec'], dtype=[cat1[col].dtype for col in cat1.colnames] + ['i8', 'f8'])
-
-    for i, c2 in enumerate(radec2):
-        sep = radec1.separation(c2)
-
-        mask = sep < max_sep
-        nearby_galaxies = cat1[mask]
-        if len(nearby_galaxies) > 0:
-            sn_id = cat2[i]['objid']
-            separations = sep[mask].arcsec
-            nearby_galaxies['Separation_arcsec'] = separations
-            nearby_galaxies['objid'] = sn_id  # Add the SN_ID to the nearby galaxies table
-            result_table = vstack([result_table, nearby_galaxies])
-
-        if test:
-            if i>300:
-                break
-    end = time.time()
-    print(end-start)
-    result_table_final = join(result_table, cat2, keys='objid')
-    result_table_final.write('nearby_galaxies_all_supernovae_with.fits', overwrite=True)
-
-#separations(gama_file, ztf_file, max_sep=465, test = False)
-#exit()
 
 def positional_crossmatch(cat_file1, cat_file2, dim, max_sep, max_sep3d=None, cosmology = Planck18,frame = 'icrs', verbose = True):
 
@@ -79,7 +40,7 @@ def positional_crossmatch(cat_file1, cat_file2, dim, max_sep, max_sep3d=None, co
         match = hstack((cat1[a2b], cat2[b2a]))
         match['sep_arcsec'] = sep.arcsec
 
-        output_path = f"crossmatches/crossmatch_GAMA_ZTF_maxsep_{max_sep}arcsec_dim{dim}.fits"
+        output_path = f"crossmatches/positional/crossmatch_GAMA_ZTF_maxsep_{max_sep}arcsec_dim{dim}.fits"
         match.write(output_path, overwrite = True)
         return match
     if dim ==3:
@@ -116,7 +77,7 @@ def positional_crossmatch(cat_file1, cat_file2, dim, max_sep, max_sep3d=None, co
         match1 = hstack((cat1[a2b], cat2[b2a]))
         match1['sep_arcsec'] = sep.arcsec
 
-        output_path = f"crossmatches/crossmatch_GAMA_ZTF_maxsep_{max_sep}arcsec_dim{dim}_onsky.fits"
+        output_path = f"crossmatches/junk/crossmatch_GAMA_ZTF_maxsep_{max_sep}arcsec_dim{dim}_onsky.fits"
         match1.write(output_path, overwrite = True)
 
         #### 3D ####
@@ -147,6 +108,40 @@ def positional_crossmatch(cat_file1, cat_file2, dim, max_sep, max_sep3d=None, co
 #positional_crossmatch(gama_file, ztf_file, dim=2, max_sep = 465)
 #exit()
 
+def separations(cat_file1, cat_file2, max_sep, frame = 'icrs', test = False):
+    start = time.time()
+    cat1 = Table.read(cat_file1)
+    cat2 = Table.read(cat_file2)
+    radec1 = co.SkyCoord(cat1['RAcen'], cat1['Deccen'], frame = frame, unit='deg') # GAMA (ICRS): (ra, dec) in deg
+    radec2 = co.SkyCoord(cat2['ra']*u.deg, cat2['declination']*u.deg, frame=frame) # ZTF (ICRS): (ra, dec) in deg
+
+    max_sep = max_sep*u.arcsec
+
+    result_table = Table(names=cat1.colnames + ['objid', 'Separation_arcsec'], dtype=[cat1[col].dtype for col in cat1.colnames] + ['i8', 'f8'])
+
+    for i, c2 in enumerate(radec2):
+        sep = radec1.separation(c2)
+
+        mask = sep < max_sep
+        nearby_galaxies = cat1[mask]
+        if len(nearby_galaxies) > 0:
+            sn_id = cat2[i]['objid']
+            separations = sep[mask].arcsec
+            nearby_galaxies['Separation_arcsec'] = separations
+            nearby_galaxies['objid'] = sn_id  # Add the SN_ID to the nearby galaxies table
+            result_table = vstack([result_table, nearby_galaxies])
+
+        if test:
+            if i>300:
+                break
+    end = time.time()
+    print(end-start)
+    result_table_final = join(result_table, cat2, keys='objid')
+    result_table_final.write('crossmatches/dlr/nearby_galaxies_all_supernovae_with.fits', overwrite=True)
+
+#separations(gama_file, ztf_file, max_sep=465, test = False)
+#exit()
+
 #! I'm ging to do a rough crossmatch between all catalogues, to have a big catalogue with all the information. I propose to create a function with dictionaries
 #! to create this kind of catalogue quikly
 
@@ -166,7 +161,7 @@ gama_SFR_file = "data/MagPhysv06.fits" # 'CATAID', 'Z', 'sSFR_0_1Gyr_best_fit', 
 
 #separations = [1.5]#,5,47,465]
 #matched_file = [f"crossmatches/crossmatch_GAMA_ZTF_maxsep_{sep}arcsec_dim2.fits" for sep in separations] # 'uberID', 'RAcen', 'Deccen', 'CATAID', 'Z', 'ra', 'declination', 'redshift'
-matched_file = "crossmatches/crossmatch_GAMA_ZTF_maxsep_465arcsec_dim2.fits" # 'uberID', 'RAcen', 'Deccen', 'CATAID', 'Z', 'ra', 'declination', 'redshift'
+matched_file = "crossmatches/positional/crossmatch_GAMA_ZTF_maxsep_465arcsec_dim2.fits" # 'uberID', 'RAcen', 'Deccen', 'CATAID', 'Z', 'ra', 'declination', 'redshift'
 
 #! I need 3 catalogues:
     #! plot 1: (colour - mass) + host(colour - mass) EASY, DONE
@@ -184,41 +179,40 @@ selected_science = science_cat[['uberID', 'RAcen', 'Deccen', 'CATAID', 'flux_it'
 selected_mass = mass_cat[['uberID', 'Z', 'logmstar', 'dellogmstar', 'mstar', 'delmstar', 'gminusi', 'delgminusi', 'gminusi_stars', 'delgminusi_stars', 'uminusr', 'deluminusr']]
 selected_sersic_SDSS = sersic_cat_SDSS[['CATAID', 'RA', 'DEC', 'GALINDEX_i', 'GALINDEX_g', 'GALINDEX_u', 'GALINDEX_r', 'GALMAG_i', 'GALMAG_g', 'GALMAG_u', 'GALMAG_r']]
 selected_sersic_VIKING = sersic_cat_VIKING[['CATAID', 'RA', 'DEC', 'GALINDEX_Z', 'GALMAG_Z']]
-
 selected_SFR = SFR_cat[['CATAID', 'sSFR_0_1Gyr_best_fit', 'SFR_0_1Gyr_best_fit']]
-selected_matched = matched_cat[['uberID', 'ra', 'declination', 'redshift']]
+
+selected_matched = matched_cat[['uberID', 'ra', 'declination', 'redshift', 'sep_arcsec']]
 
 
 # plot 1:
-'''
-colour_mass_cat = selected_mass
-colour_mass_cat.write('crossmatches/join_colour_mass_cat_all.fits', overwrite = True)
-matched_color_mass_cat = join(selected_mass, selected_matched, keys='uberID')
-matched_color_mass_cat.write('crossmatches/join_colour_mass_cat_matched465.fits', overwrite = True)
 
-'''
+colour_mass_cat = selected_mass
+colour_mass_cat.write('crossmatches/specifics/colour_mass_all.fits', overwrite = True)
+matched_color_mass_cat = join(selected_mass, selected_matched, keys='uberID')
+matched_color_mass_cat.write('crossmatches/specifics/colour_mass_matched465.fits', overwrite = True)
+
 # plot 2:
 
 colour_sersic_cat_viking1 = join(selected_science, selected_mass, keys='uberID')
 colour_sersic_cat_viking = join(colour_sersic_cat_viking1, selected_sersic_VIKING, keys='CATAID')
-colour_sersic_cat_viking.write('crossmatches/join_colour_sersic_cat_all_VIKING.fits', overwrite = True)
+colour_sersic_cat_viking.write('crossmatches/specifics/colour_sersic_all_VIKING.fits', overwrite = True)
 
 matched_colour_sersic_cat_viking = join(colour_sersic_cat_viking, selected_matched, keys='uberID')
-matched_colour_sersic_cat_viking.write('crossmatches/join_colour_sersic_cat_matched465_VIKING.fits', overwrite = True)
+matched_colour_sersic_cat_viking.write('crossmatches/specifics/colour_sersic_matched465_VIKING.fits', overwrite = True)
 
 colour_sersic_cat_sdss1 = join(selected_science, selected_mass, keys='uberID')
 colour_sersic_cat_sdss = join(colour_sersic_cat_sdss1, selected_sersic_SDSS, keys='CATAID')
-colour_sersic_cat_sdss.write('crossmatches/join_colour_sersic_cat_all_SDSS.fits', overwrite = True)
+colour_sersic_cat_sdss.write('crossmatches/specifics/colour_sersic_all_SDSS.fits', overwrite = True)
 
 matched_colour_sersic_cat_sdss = join(colour_sersic_cat_sdss, selected_matched, keys='uberID')
-matched_colour_sersic_cat_sdss.write('crossmatches/join_colour_sersic_cat_matched465_SDSS.fits', overwrite = True)
+matched_colour_sersic_cat_sdss.write('crossmatches/specifics/colour_sersic_matched465_SDSS.fits', overwrite = True)
 
-exit()
+
 # plot 3:
 
 colour_SFR_cat1 = join(selected_science, selected_mass, keys='uberID')
 colour_SFR_cat = join(colour_SFR_cat1, selected_SFR, keys='CATAID')
-colour_SFR_cat.write('crossmatches/join_colour_SFR_cat_all.fits', overwrite = True)
+colour_SFR_cat.write('crossmatches/specifics/colour_SFR_all.fits', overwrite = True)
 
 matched_colour_SFR_cat = join(colour_SFR_cat, selected_matched, keys='uberID')
-matched_colour_SFR_cat.write('crossmatches/join_colour_SFR_cat_matched465.fits', overwrite = True)
+matched_colour_SFR_cat.write('crossmatches/specifics/colour_SFR_matched465.fits', overwrite = True)
